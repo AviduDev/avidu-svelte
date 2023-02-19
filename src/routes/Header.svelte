441b593764d947ab2dbcb72a789a0f3d8b289849
AviduDev <avidudev@gmail.com>
2023-02-19 07:11:26 +0530

    first commit

---
+++ src/routes/Header.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header>
+
+	<nav>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/projects') ? 'page' : undefined}>
+				<a href="/projects">Projects</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/services') ? 'page' : undefined}>
+				<a href="/services">Services</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/about">About</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/contact') ? 'page' : undefined}>
+				<a href="/contact">Contact</a>
+			</li>
+		</ul>
+	</nav>
+
+</header>
+
+<style>
+	
+</style>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,53 @@
+<script></script>
+
+<svelte:head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<meta name="robots" content="index, follow" />
+
+	<title>Website Designer and Frontend Developer from Sri Lanka</title>
+	<meta name="description" content="Avidu is a good website designer from sri lanka" />
+	<meta name="keywords" content="Web Design, Web Development, Frontend Development" />
+	<meta name="author" content="Avidu Sampath" />
+
+	<meta property="og:type" content="website" />
+	<meta
+		property="og:image"
+		content="https://res.cloudinary.com/avidu/image/upload/v1672635670/1JxxrWlPeEgNUPLtiQbSG8eGiGea8ijJb_u8vibc.png"
+	/>
+	<meta property="og:title" content="Website Designer and Frontend Developer from Sri Lanka" />
+	<meta property="og:description" content="Avidu is a good website designer from sri lanka" />
+	<meta name="og:url" content="https://avidu.me" />
+	<meta property="og:locale" content="es_ES" />
+	<meta property="og:url" content="https://avidu.me" />
+
+	<meta name="twitter:title" content="Website Designer and Frontend Developer from Sri Lanka" />
+	<meta name="twitter:description" content="Avidu is a good website designer from sri lanka" />
+	<meta
+		name="twitter:image"
+		content="https://res.cloudinary.com/avidu/image/upload/v1672635670/1JxxrWlPeEgNUPLtiQbSG8eGiGea8ijJb_u8vibc.png"
+	/>
+	<meta name="twitter:domain" content="https://avidu.me" />
+	<meta name="twitter:card" content="summary_large_image" />
+
+	{@html `<script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "website",
+              "name": "Website Designer and Frontend Developer from Sri Lanka",
+              "description": "Avidu is a good website designer from sri lanka",
+              "image": "https://res.cloudinary.com/avidu/image/upload/v1672635670/1JxxrWlPeEgNUPLtiQbSG8eGiGea8ijJb_u8vibc.png"
+              "url": "https://avidu.me",
+              "author" : {
+                "@type" : "Person",
+                "name" : "Avidu Sampath"
+            },
+            }
+    </script>`}
+</svelte:head>
+
+<section>
+    <h1>Contact page</h1>
+</section>
+
+<style></style>
